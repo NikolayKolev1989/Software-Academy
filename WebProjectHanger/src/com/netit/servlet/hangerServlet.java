a9e1499c3bc87cfae8e8d28f2682d1cd64c99729
Nikolay Kolev <nikolay.kolev1989@gmail.com>
2021-01-30 15:54:28 +0200

    add functionality, table html formatting and english words

--- WebProjectHanger/src/com/netit/servlet/hangerServlet.java
+++ WebProjectHanger/src/com/netit/servlet/hangerServlet.java
@@ -1,6 +1,7 @@
 package com.netit.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Random;
 
 import javax.servlet.ServletException;
@@ -9,9 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import main.HangerWebVersion;
-import main.Task_Hanger_WorkingVersion;
-
 /**
  * Servlet implementation class hangerServlet
  */
@@ -19,15 +17,15 @@ import main.Task_Hanger_WorkingVersion;
 
 public class hangerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private HangerWebVersion hanger = new HangerWebVersion();
 	private int mistaкeCounter = 0;
 	private String wordToGuess;
 	private String result[];
-	private String finalString = "";
+	private String resultToString = "";
 	private boolean isWordGuessed = false;
-	private static String wordList[] = { "бесилка", "локомотив", "маймуна", "гардероб", "прозорец", "акумулатор",
-			"катедрала", "анатомия", "обектив", "абориген", "хромозома", "манатарка", "контейнер", "уравнение",
-			"зъболекар", "намаление", "индикатор", "пробиване", "дъждобран", "отпечатък", "дървесина", "галактика" };
+	private static String wordList[] = { "hanger", "locomotive", "monkey", "wardrobe", "window", "accumulator",
+			"cathedral", "anatomy", "objective", "chromosome", "container", "equation",
+			"dentist", "discount", "indicator", "puncture", "raincoat", "fingerprint"};
+
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -53,7 +51,7 @@ public class hangerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.getRequestDispatcher("game.html").forward(request, response);
-		System.out.println(wordToGuess);
+//		System.out.println(wordToGuess);
 
 	}
 
@@ -67,6 +65,7 @@ public class hangerServlet extends HttpServlet {
 		request.getRequestDispatcher("game.html").forward(request, response);
 		String entry = request.getParameter("charToGuess");
 		System.out.println(entry);
+		PrintWriter out = response.getWriter();
 
 		// Проверка дали символа въведен от потребителя съществува в думата
 		boolean isCharFound = false;
@@ -83,17 +82,54 @@ public class hangerServlet extends HttpServlet {
 		}
 		
 		// Принтира какво се е разкрило от думата
-		finalString="";
+		resultToString="";
 		for (String symbol : result) {
-			finalString += symbol + " ";
+			resultToString += symbol + " ";
 		}
 		
 		// Проверява дали са разкрити всички звездички
-		if (!finalString.contains("*")) {
+		if (!resultToString.contains("*")) {
 			isWordGuessed = true;
 		} else {
-			System.out.println(finalString);
+			System.out.println(resultToString);
 		}
 		System.out.println("Грешки " + "(" + mistaкeCounter + ")");
+		
+		out.append("<!DOCTYPE html>\r\n"
+				+ "<html>\r\n"
+				+ "	<head>\r\n"
+				+ "	<meta charset=\"UTF-8\">\r\n"
+				+ "	<title>Hanger</title>\r\n"
+				+ "	</head>\r\n"
+				+ "	<body bgcolor=\"lightblue\">\r\n"
+				+ "		<form method=\"post\" action=\"hanger\">\r\n"
+				+ "			<table width=\"25%\">\r\n"
+				+ "				<thead>\r\n"
+				+ "					<tr>\r\n"
+				+ "                  	<th colspan=\"2\" align=\"right\"><h2>Hanger</h2></th>\r\n"
+				+ "				</tr>\r\n"
+				+ "				</thead>\r\n"
+				+ "				<tbody>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td align=\"right\" >Enter character:</td>\r\n"
+				+ "						<td align=\"center\"><input type=\"text\" name=\"charToGuess\" id=\"charToGuess\" maxlength=\"1\" size=\"5\"></td>\r\n"
+				+ "						<td align=\"left\" rowspan=\"2\"><textarea rows=\"5\" cols=\"22\" name=\"history\" readonly=\"readonly\"> " + resultToString + "</textarea></td>\r\n"
+				+ "					</tr>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td></td>\r\n"
+				+ "						<td align=\"center\"><input type=\"submit\" name=\"checkButton\" value=\"Check\" ></td>\r\n"
+				+ "					</tr>\r\n"
+				+ "				</tbody>\r\n"
+				+ "			</table>\r\n"
+				+ "		</form>\r\n"
+				+ "	</body>\r\n"
+				+ "</html>");
+		
 	}
 }
+
+
+
+
+
+
